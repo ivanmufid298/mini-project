054ba24e18fdaf88a4bf48136b3289f15eacb0d6
ivan mufid <ivanmohammad2355@gmail.com>
2022-05-11 21:22:03 +0700

    Fix

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
     <Navbar />
-    <h2>Product List</h2>
+    <ProductList />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
+import ProductList from "@/components/ProductList.vue";
 
 export default {
   name: "Products",
   components: {
     Navbar,
+    ProductList,
   },
 };
 </script>
